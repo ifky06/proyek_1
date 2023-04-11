22fd607eb9656c368f965e48bab0af92ffa68d93
Sukma Gladys V <sukmagladysv@gmail.com>
2023-04-11 10:56:56 +0700

    Detail transaksi keluar membuat model, migration

---
+++ app/Http/Controllers/DetailTransaksiKeluarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Detail_transaksi_keluar;
+use Illuminate\Http\Request;
+
+class DetailTransaksiKeluarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Detail_transaksi_keluar  $detail_transaksi_keluar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Detail_transaksi_keluar $detail_transaksi_keluar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Detail_transaksi_keluar  $detail_transaksi_keluar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Detail_transaksi_keluar $detail_transaksi_keluar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Detail_transaksi_keluar  $detail_transaksi_keluar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Detail_transaksi_keluar $detail_transaksi_keluar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Detail_transaksi_keluar  $detail_transaksi_keluar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Detail_transaksi_keluar $detail_transaksi_keluar)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_04_11_034444_create_detail_transaksi_keluars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_transaksi_keluar', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_transaksi')->nullable();
+            $table->bigInteger('id_barang')->nullable();
+            $table->integer('qty')->nullable();
+            $table->float('harga')->nullable();
+            $table->float('grandtotal')->nullable();
+            $table->bigInteger('id_users')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_transaksi_keluar');
+    }
+};
