0f344b6a327c8982ae5224ab9363229581d254cf
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2023-05-08 20:39:45 +0700

    CRUD Satuan

---
+++ resources/views/satuan/satuan.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.template')
+
+@section('content')
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Satuan</h1>
+                </div>
+{{--                <div class="col-sm-6">--}}
+{{--                    <ol class="breadcrumb float-sm-right">--}}
+{{--                        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>--}}
+{{--                        <li class="breadcrumb-item active">Dashboard</li>--}}
+{{--                    </ol>--}}
+{{--                </div>--}}
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-body">
+                <a href="{{url('satuan/create')}}" class="btn btn-sm btn-success my-2">Tambah Data</a>
+                <form action="{{url('satuan')}}" method="get">
+                    <div class="input-group mb-3 w-25">
+                        <input type="text" name="search" class="form-control" placeholder="Search"
+                               value="{{request()->search}}">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary" type="submit">Search</button>
+                        </div>
+                    </div>
+                </form>
+                <table class="table table-bordered table-striped mb-3">
+                    <thead>
+                    <tr>
+                        <th>Nama Satuan</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($data as $row)
+                        <tr>
+                            <td>{{$row->satuan}}</td>
+                            <td>
+                                <a href="{{route('satuan.edit', $row->id)}}" class="btn btn-primary btn-sm">Edit</a>
+                                <form action="{{route('satuan.destroy', $row->id)}}" method="post" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger btn-sm" onclick="return confirm('Apakah anda yakin?')">Delete</button>
+                                </form>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                {{ $data->links() }}
+            </div>
+        </div>
+        <!-- /.card -->
+
+    </section>
+@endsection
+
+@push('css')
+
+@endpush
+
+@push('scripts')
+
+    {{--    <script>--}}
+    {{--        alert('Selamat Datang');--}}
+    {{--    </script>--}}
+
+@endpush
