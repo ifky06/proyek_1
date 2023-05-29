925d6630892cb0d685c2a2539037c4f79497200a
Ahmad Rifki Fauzi <ahmadrifki3403@gmail.com>
2023-05-29 19:00:54 +0700

    add onDelete cascade to table relation barang, detail_masuk, and detail_keluar

---
+++ database/migrations/2023_05_29_114619_add_cascade_to_barang.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('barang', function (Blueprint $table) {
+            $table->dropForeign(['id_kategori']);
+            $table->dropForeign(['id_pemasok']);
+            $table->dropForeign(['id_satuan']);
+            $table->foreign('id_kategori')->references('id')->on('kategori')->onDelete('cascade');
+            $table->foreign('id_pemasok')->references('id')->on('pemasok')->onDelete('cascade');
+            $table->foreign('id_satuan')->references('id')->on('satuan')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('barang', function (Blueprint $table) {
+            $table->dropForeign(['id_kategori']);
+            $table->dropForeign(['id_pemasok']);
+            $table->dropForeign(['id_satuan']);
+            $table->foreign('id_kategori')->references('id')->on('kategori');
+            $table->foreign('id_pemasok')->references('id')->on('pemasok');
+            $table->foreign('id_satuan')->references('id')->on('satuan');
+        });
+    }
+};
